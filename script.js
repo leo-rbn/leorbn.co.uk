a61e3823bc16eb4808832af15c1685d7c0657be6
leo-rbn <269581255+leo-rbn@users.noreply.github.com>
2026-03-19 23:27:38 +0000

    First mockup

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("sidebarToggle").addEventListener("click", function() {
+    document.getElementById("sidebar").classList.toggle("show");
+    document.getElementById("sidebarToggle").classList.toggle("hidden");
+});
